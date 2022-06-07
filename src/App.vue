d198598f141980937cb65d77706f8df3c072d1cc
drlhc <huachaoliangdr@foxmail.com>
2022-06-07 14:01:36 +0800

    add：wangeditor：github.io。

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <AntDesign/>
+  <WangEditor/>
+<!--  <HelloWorld msg="Welcome to Your Vue.js App"/>-->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import WangEditor from './components/WangEditor.vue'
+import AntDesign from './components/AntDesign.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    WangEditor,
+    AntDesign,
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import AntD from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
 
-createApp(App).mount('#app')
+
+const app = createApp(App)
+
+app.use(AntD)
+
+app.mount('#app')
